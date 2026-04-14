4322266d9787db2f5cff34be52ca4f4bd50a7526
logic958 <tinocrain@gmail.com>
2026-04-14 09:46:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-let menuOpen = false;
-
-function toggleMenu() {
-    const menu = document.getElementById("sideMenu");
-
-    if(menuOpen) {
-        menu.style.right = "-250px";
-        menuOpen = false;
-    } else {
-        menu.style.right = "0";
-        menuOpen = true;
-    }
-}
+window.scrollToServices = function () {
+    document.getElementById("services").scrollIntoView({ 
+        behavior: "smooth" 
+    });
+};
